e053609857d1981b471f3ebdb8092852293ff6cd
ulofiai <monsterking@tutamail.com>
2026-08-07 20:39:21 +0800

    test(hstore): cover public clear and recreate lifecycle
    Signed-off-by: ulofiai <monsterking@tutamail.com>

--- hugegraph-server/hugegraph-hstore/src/test/java/org/apache/hugegraph/backend/store/hstore/HstoreSessionsImplTest.java
+++ hugegraph-server/hugegraph-hstore/src/test/java/org/apache/hugegraph/backend/store/hstore/HstoreSessionsImplTest.java
@@ -19,11 +19,15 @@ package org.apache.hugegraph.backend.store.hstore;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.nio.charset.StandardCharsets;
-import java.util.Set;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.hugegraph.config.HugeConfig;
+import org.apache.hugegraph.pd.common.PDException;
+import org.apache.hugegraph.pd.grpc.Metapb;
+import org.apache.hugegraph.store.HgStoreSession;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -41,28 +45,59 @@ public class HstoreSessionsImplTest {
     }
 
     @Test
-    public void testClearInitializedGraphStateRepeatedly() throws Exception {
-        Field initializedGraphField = HstoreSessionsImpl.class.getDeclaredField(
-                "infoInitializedGraph");
-        initializedGraphField.setAccessible(true);
-        @SuppressWarnings("unchecked")
-        Set<String> initializedGraphs =
-                (Set<String>) initializedGraphField.get(null);
-
-        Method clearInitializedGraph =
-                HstoreSessionsImpl.class.getDeclaredMethod(
-                        "clearInitializedGraph", String.class);
-        clearInitializedGraph.setAccessible(true);
-
+    public void testClearAndRecreateGraphRepeatedly() throws Exception {
         String graphName = "hugegraph/hstore-clear-test";
+        AtomicInteger graphRegistrations = new AtomicInteger();
+        AtomicInteger storeGraphDeletes = new AtomicInteger();
+        AtomicInteger pdGraphDeletes = new AtomicInteger();
+        HgStoreSession storeSession = (HgStoreSession) Proxy.newProxyInstance(
+                HgStoreSession.class.getClassLoader(),
+                new Class<?>[]{HgStoreSession.class},
+                (proxy, method, args) -> {
+                    Assert.assertEquals("deleteGraph", method.getName());
+                    Assert.assertArrayEquals(new Object[]{graphName}, args);
+                    storeGraphDeletes.incrementAndGet();
+                    return true;
+                });
+        HstoreSessionsImpl.GraphStoreClient client =
+                new HstoreSessionsImpl.GraphStoreClient() {
+                    @Override
+                    HgStoreSession openSession(String name) {
+                        Assert.assertEquals(graphName, name);
+                        return storeSession;
+                    }
+
+                    @Override
+                    void setGraph(Metapb.Graph graph) {
+                        Assert.assertEquals(graphName, graph.getGraphName());
+                        graphRegistrations.incrementAndGet();
+                    }
+
+                    @Override
+                    void delGraph(String name) throws PDException {
+                        Assert.assertEquals(graphName, name);
+                        pdGraphDeletes.incrementAndGet();
+                        throw new PDException(0, "exercise clear() finally block");
+                    }
+                };
+        HugeConfig config = new HugeConfig(Collections.emptyMap());
+        HstoreSessionsImpl sessions = new HstoreSessionsImpl(
+                config, "hugegraph", "hstore-clear-test", client);
+
         try {
+            sessions.open();
+            Assert.assertEquals(1, graphRegistrations.get());
+
             for (int i = 0; i < 50; i++) {
-                Assert.assertTrue(initializedGraphs.add(graphName));
-                clearInitializedGraph.invoke(null, graphName);
-                Assert.assertFalse(initializedGraphs.contains(graphName));
+                sessions.clear();
+                sessions.open();
+                Assert.assertEquals(i + 2, graphRegistrations.get());
             }
+
+            Assert.assertEquals(50, storeGraphDeletes.get());
+            Assert.assertEquals(50, pdGraphDeletes.get());
         } finally {
-            initializedGraphs.remove(graphName);
+            sessions.clear();
         }
     }
 }
